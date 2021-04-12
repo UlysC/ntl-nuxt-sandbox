0959f3dbeb11f5c5f1dcba3dd934e024a08519e2
UlysC <ulys.chambard@gmail.com>
2021-04-12 16:25:41 +0200

    fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,38 @@
 <template>
   <section>
-    <form action="POST" name="privacy" data-netlify="true" netlify-honeypot="bot-field">
-      <label>sometext: <input type="text" name="letext"></label>
+    <form
+      action="POST"
+      name="privacy"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+      @submit="submit"
+    >
+      <label>sometext: <input type="text" name="letext" v-model="text"/></label>
+      <button type="submit">u n s u b s c r i b e</button>
     </form>
   </section>
 </template>
 
 <script>
-export default {
+export default {  
+
+  data: () => ({
+    text: ""
+  }),
+  methods: {
+    submit(){
+      fetch('/',{
+        method: 'post',
+        body: {
+          text: encodeURIComponent(this.text)
+        }
+      })
+      .then(res => console.log(res))
+      .catch(err=> console.log(err))
+    }
+  }
 
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
