958993a85956850c7cd3021b8dcc8166f998a5b3
UlysC <ulys.chambard@gmail.com>
2021-04-12 16:18:47 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <form action="POST" name="privacy" data-netlify="true" netlify-honeypot="bot-field">
+      <label>sometext: <input type="text" name="letext"></label>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
